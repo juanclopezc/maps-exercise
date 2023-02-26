c2f57d007822e418a11957fa12817d761011d2a4
Juan López <juanclopezc86@gmail.com>
2023-02-26 11:08:10 -0600

    Add: header styles

--- src/app/private/layout/header/header.module.ts
+++ src/app/private/layout/header/header.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HeaderComponent } from './header.component';
 
 
@@ -9,7 +17,14 @@ import { HeaderComponent } from './header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatToolbarModule
   ],
   exports: [HeaderComponent]
 })
